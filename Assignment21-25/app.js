d7cc7c7abff6e5aec57561b3339515a8cc645ed2
Nazbibi97 <nazbibi103@gmail.com>
2024-10-29 18:44:57 +0500

    JS chapters 21-40

---
+++ Assignment21-25/app.js
@@ -0,0 +1,30 @@
+// ////////////// ----1.
+/*var userFirstName = prompt ("Please Enter your first name");
+var userLastName = prompt ("Enter your last name");
+ var fullName = userFirstName+" " +userLastName
+
+ function greet(){
+    alert("Helllo! " + fullName)
+ }
+ greet()*/
+
+// // ///////// -----2.
+// var mobileArr = ["samsung galaxy s6 edge plus" ,"iphone 14 pro ", "realme c50" ,];
+// var userFavMob = prompt (`"Which is your favorite phone model ?  " \n 1.Samsung Galaxy S6 Edge Plus \n 2.iPhone 14 pro \n 3.Realme C50` );
+// userFavMob= userFavMob.toLowerCase()
+// document.write("Your Favorite phone is: " + userFavMob + " length of string: "+ mobileArr.indexOf(userFavMob));
+
+
+// ////////------ 3. 
+// var pakStr = "pakistan i "
+// document.write("String: Pakistani"+ "</br>" +" Index of 'n': " + pakStr.indexOf("n") )
+
+
+//  //////----4.
+// var helloStr = "Hello World "
+// document.write("String: Hello World"+ "</br>" +"Last Index of 'l': " + helloStr.lastIndexOf("l"))
+
+
+// //////----5.
+var pakStr = "Pakistani"
+document.write ("String: Pakistsani" + "</br>" +"Character at index 3 :" + pakStr.charAt(3))
